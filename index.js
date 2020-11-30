f447c6c52a8e7aa9ecc8afab60340fba89eb83b6
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-30 01:51:03 -0500

    added prompt and generate functions

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your project title?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Enter description: ',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'Enter installation instructions: ',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'Enter usage information: ',
-            name: 'usage',
-        },
-        {
-            type: 'input',
-            message: 'Enter contribution guidelines: ',
-            name: 'contribution',
-        },
-        {
-            type: 'input',
-            message: 'Enter test instructions: ',
-            name: 'test',
-        },
-        {
-            type: 'list',
-            message: 'Choose License: ',
-            name: 'license',
-            choices: ['BSD', 'MIT', 'GPL'],
-        },
+const writeFile = util.promisify(fs.writeFile);
 
-    ])
-    .then((response) =>
-        response.confirm === response.password
-            ? console.log('Success!')
-            : console.log('You forgot your password already?!')
-    );
+const promptUser = () =>
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your project title?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'Enter description: ',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'Enter installation instructions: ',
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: 'Enter usage information: ',
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: 'Enter contribution guidelines: ',
+                name: 'contribution',
+            },
+            {
+                type: 'input',
+                message: 'Enter test instructions: ',
+                name: 'test',
+            },
+            {
+                type: 'list',
+                message: 'Choose License: ',
+                name: 'license',
+                choices: ['BSD', 'MIT', 'GPL'],
+            },
+
+        ]);
+
+
+const generateHTML = (answers) => '';
+
+promptUser()
+.then((answers) => writeFile('index.html', generateHTML(answers)))
+.then(() => console.log('Successfully wrote to index.html'))
+.catch((err) => console.error(err));
+
